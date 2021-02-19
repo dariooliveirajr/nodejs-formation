74bb727c60f240d4ee4ef20173644f5e09beeebe
Dario de Oliveira Junior <dariooljr@gmail.com>
2021-02-19 15:25:49 -0300

    Creating the Pagination System

--- guiapress/articles/articlesController.js
+++ guiapress/articles/articlesController.js
@@ -79,6 +79,34 @@ router.post("/articles/update", (req, res) => {
         .catch(err => {
             res.redirect("/");
         });
-})
+});
+
+router.get("/articles/page/:num", (req, res) => {
+    var page = req.params.num;
+    var offset = 0;
+
+    if(isNaN(page) || page == 1) {
+        offset = 0;
+    } else {
+        offset = (parseInt(page) - 1) * 4;
+    }
+
+    Article.findAndCountAll({limit: 4, offset, order: [['id','DESC']]})
+        .then(articles => {
+
+            var next;
+            if(offset + 4 >= articles.count) {
+                next = false;
+            } else {
+                next = true;
+            }
+
+            var result = {page: parseInt(page), articles, next}
+
+            Category.findAll().then(categories => {
+                res.render("admin/articles/page",{result, categories})
+            });
+        })
+});
 
 module.exports = router;
