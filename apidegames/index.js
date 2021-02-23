a324e771d9373eb5839c8ee043c592696ab23772
Dario de Oliveira Junior <dariooljr@gmail.com>
2021-02-23 18:02:39 -0300

    Creating the authentication system

--- apidegames/index.js
+++ apidegames/index.js
@@ -2,11 +2,35 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+const JWTsecret = "akldsfhuiwahefuownofnoajfej"
 
 app.use(cors());
-
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+function auth(req, res, next) {
+    const authToken = req.headers['authorization'];
+    if(authToken != undefined) {
+        const bearer = authToken.split(' ');
+        var token = bearer[1];
+
+        jwt.verify(token,JWTsecret, (err, data) => {
+            if(err) {
+                res.status(401);
+                res.json({err: "Token inválido!"})
+            } else {
+                req.token = token;
+                req.loggedUser = {id: data.id, email: data.email};
+                next();
+            }
+        });
+    } else {
+        res.status(401);
+        res.json({err: "Token inválido!"});
+    }
+}
 
 var DB = {
     games: [
@@ -28,15 +52,29 @@ var DB = {
             year: 2012,
             price: 20
         }
+    ],
+    users: [
+        {
+            id: 1,
+            name: "Dario Olviveira",
+            email: "dariooljr@gmail.com",
+            password: "123"
+        },
+        {
+            id: 20,
+            name: "Guilherme",
+            email: "guigg@gmail.com",
+            password: "java123"
+        }
     ]
 }
 
-app.get("/games",(req, res) => {
+app.get("/games",auth,(req, res) => {
     res.statuscode = 200;
     res.json(DB.games);
 });
 
-app.get("/game/:id", (req, res) => {
+app.get("/game/:id",auth,(req, res) => {
     if(isNaN(req.params.id)){
         res.sendStatus(400);
     } else {
@@ -52,7 +90,7 @@ app.get("/game/:id", (req, res) => {
     }
 });
 
-app.post("/game", (req, res) => {
+app.post("/game",auth,(req, res) => {
     var { title, price, year } = req.body;
     DB.games.push({
         id: 2323,
@@ -63,7 +101,7 @@ app.post("/game", (req, res) => {
     res.sendStatus(200);
 });
 
-app.delete("/game/:id", (req, res) => {
+app.delete("/game/:id",auth,(req, res) => {
     if(isNaN(req.params.id)){
         res.sendStatus(400);
     } else {
@@ -79,7 +117,7 @@ app.delete("/game/:id", (req, res) => {
     }
 });
 
-app.put("/game/:id", (req, res) => {
+app.put("/game/:id",auth,(req, res) => {
     if(isNaN(req.params.id)){
         res.sendStatus(400);
     } else {
@@ -106,6 +144,36 @@ app.put("/game/:id", (req, res) => {
         } else {
             res.sendStatus(404);
         }
+    }
+});
+
+app.post("/auth",auth,(req, res) => {
+    var {email, password} = req.body;
+    if(email != undefined) {
+        var user = DB.users.find(u => u.email == email);
+
+        if(user != undefined) {
+            if(user.password == password) {
+                jwt.sign({id: user.id, email: user.email},JWTsecret,{expiresIn:'48h'}, (err, token) => {
+                    if(err) {
+                        res.status(400);
+                        res.json({err: "Falha interna"});
+                    } else {
+                        res.status(200);
+                        res.json({token: token});
+                    }
+                });
+            } else {
+                res.status(401);
+                res.json({err: "Credenciais inválidas!"})
+            }
+        } else {
+            res.status(404);
+            res.json({err: "O E-mail enviado não existe na base de dados!"});
+        }
+    } else {
+        res.status(400);
+        res.json({err: "O E-mail enviado é inválido"});
     }
 });
 
