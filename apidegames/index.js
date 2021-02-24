5383e48da55d27edb3ea1688cac03dbeb78c3bd8
Dario de Oliveira Junior <dariooljr@gmail.com>
2021-02-24 18:51:52 -0300

    Applying HATEOAS

--- apidegames/index.js
+++ apidegames/index.js
@@ -70,8 +70,27 @@ var DB = {
 }
 
 app.get("/games",auth,(req, res) => {
+
+    var HATEOAS = [
+        {
+            href: "http://localhost:45678/game/0",
+            method: "DELETE",
+            rel: "delete_game"
+        },
+        {
+            href: "http://localhost:45678/game/0",
+            method: "GET",
+            rel: "get_game"
+        },
+        {
+            href: "http://localhost:45678/auth",
+            method: "POST",
+            rel: "login"
+        }
+    ]
+
     res.statuscode = 200;
-    res.json(DB.games);
+    res.json({ games: DB.games, _links: HATEOAS });
 });
 
 app.get("/game/:id",auth,(req, res) => {
@@ -81,9 +100,32 @@ app.get("/game/:id",auth,(req, res) => {
         var id = parseInt(req.params.id);
         var game = DB.games.find(game => game.id == id);
 
+        var HATEOAS = [
+            {
+                href: "http://localhost:45678/game/"+id,
+                method: "DELETE",
+                rel: "delete_game"
+            },
+            {
+                href: "http://localhost:45678/game/"+id,
+                method: "PUT",
+                rel: "edit_game"
+            },
+            {
+                href: "http://localhost:45678/game/"+id,
+                method: "GET",
+                rel: "get_game"
+            },
+            {
+                href: "http://localhost:45678/games",
+                method: "GET",
+                rel: "get_all_games"
+            }
+        ]
+
         if(game != undefined) {
             res.statuscode = 200;
-            res.json(game);
+            res.json({ game, _links: HATEOAS });
         } else {
             res.sendStatus(404);
         }
